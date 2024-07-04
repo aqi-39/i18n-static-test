e1b8d87317bc16543df1ad7d7256c296da0706fa
qiYang <qiyang@xiaoe-tech.com>
2024-07-04 14:52:59 +0800

    fix: test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { createVuePlugin as vue2 } from 'vite-plugin-vue2';
 
 export default defineConfig({
 	plugins: [vue2()],
+	build: {
+		outDir: 'dist/1000',
+		assetsDir: ''
+	},
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
